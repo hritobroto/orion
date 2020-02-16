10aaeaf78344cd5a225a8506d718c0e01cb73c91
hritobroto <60534062+hritobroto@users.noreply.github.com>
2020-02-16 14:37:50 +0530

    Update trial.py
    check for the video capture and change it based on the camera.
    0 for webcam
    1 for external camera

--- Ball Tracing Percentage/trial.py
+++ Ball Tracing Percentage/trial.py
@@ -1,38 +1,45 @@
-#packages
 from imutils.video import VideoStream
 import numpy as np
 import imutils
 import cv2
 
-
-#image preparation and conversion to HSV and creating mask using bitwise opereations
-img = cv2.imread('D:/self12.jpg')
-img = imutils.resize(img, width=600)
-blurred = cv2.GaussianBlur(img, (11, 11), 0)
-hsv=cv2.cvtColor(blurred,cv2.COLOR_BGR2HSV);
-low=np.array([25,52,72]);
-high=np.array([102,255,255]);
-mask=cv2.inRange(hsv,low,high);
-mask = cv2.erode(mask, None, iterations=2)
-mask = cv2.dilate(mask, None, iterations=2)
-green=cv2.bitwise_and(img,img,mask=mask);
-
-
-contour = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-contour = imutils.grab_contours(contour)
-center = None
-if len(contour) > 0:
-    # finding the largest contour and use it to draw an enclosing circle around the outer surface of the ball
-    c = max(contour, key=cv2.contourArea)
-    ((x, y), radius) = cv2.minEnclosingCircle(c)
-    M = cv2.moments(c)
-    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-    if radius > 10:
-        # drawing outline over the ball
-        cv2.circle(img, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+#video capture is set to 1 for external camera and set to 0 to use webcam
+vid=cv2.VideoCapture(1)
+while True:
+    _, frame=vid.read();
     
-ratio_green = cv2.countNonZero(mask)/(img.size/3)
-print('Percentage Green:', np.round(ratio_green*100, 2))
-cv2.imshow("images", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    #conversion to hsv
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV);
+    low=np.array([25,52,72]);
+    high=np.array([102,255,255]);
+    
+    #creating masks
+    mask=cv2.inRange(hsv,low,high);
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    green=cv2.bitwise_and(frame,frame,mask=mask);
+    
+    #finding the contours to draw a circle around the ball after detection
+    contour = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contour = imutils.grab_contours(contour)
+    center = None
+    if len(contour) > 0:
+        # finding the largest contour and use it to draw an enclosing circle around the outer surface of the ball
+        c = max(contour, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        if radius > 10:
+            # drawing outline over the ball
+            cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+        
+    ratio_green = cv2.countNonZero(mask)/(frame.size/3)
+    print('Percentage Green:', np.round(ratio_green*100, 2))
+    cv2.imshow("images", frame)
+    
+    
+    cv2.imshow("Green",green);
+    key=cv2.waitKey(1)
+    if(key==27):
+        cv2.destroyAllWindows();
+        break;
